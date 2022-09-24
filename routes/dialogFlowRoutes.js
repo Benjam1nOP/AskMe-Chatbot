8305149c916b0e5d59741304c218ad065a13f107
Benjam1nOP <lakshaydheerjain@gmail.com>
2022-09-25 05:10:12 +0530

    connect server side and dialogflow

--- routes/dialogFlowRoutes.js
+++ routes/dialogFlowRoutes.js
@@ -1,3 +1,6 @@
+'use strict';
+
+const chatbot = require('../chatbot/chatbot');
 //module is current variable
 //export is an object that will be exposed as a module
 module.exports = app => {
@@ -5,11 +8,13 @@ module.exports = app => {
    res.send({'hello': 'bsdk'}) //route handler for get request for the home page so / 
 }); 
 
-app.post('/api/df_text_query', (req, res) => {    //route handling using express
-   res.send({'do': 'text query'}) // requesting text from the bot
+app.post('/api/df_text_query', async (req, res) => {    //route handling using express
+    
+    let responses = await chatbot.textQuery(req.body.text, req.body.parameters);
+    res.send(responses[0].queryResult); // requesting text from the bot
 }); 
-
-app.post('/api/df_event_query', (req, res) => {    //setting up route
-   res.send({'do': 'event query'}) //requesting event from the bot
+app.post('/api/df_event_query', async (req, res) => {   //setting up route
+        let responses = await chatbot.eventQuery(req.body.event, req.body.parameters);
+        res.send(responses[0].queryResult); //requesting event from the bot
 }); 
 }
