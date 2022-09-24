f6e18e7bd035f8f88c72e7b295165da3dacd4fda
Benjam1nOP <lakshaydheerjain@gmail.com>
2022-09-25 01:30:02 +0530

    inital commit

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const express = require('express');
-const app = express();
+const express = require('express'); //getting the express module
+const app = express(); //creating the app
 
-app.get('/', (req, res) => {
-   res.send({'hello': 'there'})
-});
+// request and response
+app.get('/', (req, res) => {    //route handling using express
+   res.send({'hello': 'there'}) //route handler for get request for the home page so / 
+}); //
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000; //dynamic port binding for heroku
 app.listen(PORT);
